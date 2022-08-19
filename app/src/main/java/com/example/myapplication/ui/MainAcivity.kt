c42d642098b9e7741c9d2d0e84578e07a25a8548
Syifaseptiana12 <syifaseptiana.di13@gmail.com>
2022-08-19 21:37:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/MainAcivity.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainAcivity  : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
